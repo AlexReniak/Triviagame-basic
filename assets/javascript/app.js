ded1d95cf3cd10e309e48493cf8e27f1e36eb84d
Alex R <areniak147@hotmail.com>
2018-12-19 10:01:07 -0500

    making progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,59 +6,66 @@
 // on submit, answers are calculated and score is shown
 
 
-$(document).ready(function() {
-
   // variables
   var intervalId;
   //var clockRunning = false;
   var time = 10; 
+  var correctAnswers = 0;
+  var incorrectAnswers = 0
+  var totalQuestions = 6
 
-  var questions = [
-  {
-    question: "XYZ",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [1]
-  },
-  {
-    question: "ABC",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [0],
-  },
-  {
-    question: "DEF",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [3],
-  },
-  {
-    question: "GHI",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [1],
-  },
-  {
-    question: "JKL",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [2],
-  },
-  {
-    question: "MNO",
-    choices: ["ans1", "ans2", "ans3", "ans4"],
-    correctAnswer: [1]
-  }
-  ]
+  // answer array
+  var answers = ["first answer", "second answer", "third answer", "fourth answer", "fifth answer", "sixth answer" ]
+  
+
+  // quiz questions
+  // var questions = [
+  // {
+  //   question: "XYZ",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [1]
+  // },
+  // {
+  //   question: "ABC",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [0],
+  // },
+  // {
+  //   question: "DEF",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [3],
+  // },
+  // {
+  //   question: "GHI",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [1],
+  // },
+  // {
+  //   question: "JKL",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [2],
+  // },
+  // {
+  //   question: "MNO",
+  //   choices: ["ans1", "ans2", "ans3", "ans4"],
+  //   correctAnswer: [1]
+  // }
+  // ]
+
+
 
   // Timer run function
   function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    //clockRunning = true;
+
   }
 
   // submit function to be run if time = 0 or user clicks submit button
-  function submit(){
+  function submit() {
     clearInterval(intervalId);
-    //clockRunning = false;
+    alert("You answered " + correctAnswers + " out of " + totalQuestions + "correctly")
   }
-
 
   // Losing time! (decrease timer)
   // add timer to html
@@ -67,7 +74,7 @@ $(document).ready(function() {
     
     $("#timer").html("<h2>" + "Time remaining: " + time + "</h2>");
     
-    // if function for timer when it reaches zero (before submit button is clicked)
+  // if function for timer when it reaches zero (before submit button is clicked)
     if (time === 0 ) {
       alert("Time's up!");
       submit();
@@ -81,5 +88,10 @@ $(document).ready(function() {
   // if user submits before time is over, stop time and run submit function
   $("#submit-quiz").on("click", submit);  
 
-})
+  if ($("Q1answer1" === answers[0] )) {
+    correctAnswers++
+  }
 
+  else {
+    incorrectAnswers++
+  }
